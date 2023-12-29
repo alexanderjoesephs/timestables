ff13893048834e215e7baf3ed1cf0f73f64e4a02
alexanderjoeseph <alexandersonkin@gmail.com>
2023-12-29 17:30:14 +0000

    link to admin stats, got rid of code not used

--- timestablesapp/urls.py
+++ timestablesapp/urls.py
@@ -3,15 +3,15 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('create_user',views.create_user,name='create_user'),
+    
     path('login',views.user_login,name='user_login'),
     path('logout', views.logoutview, name='user_logout'),
     path('teacher_stats',views.teacher_stats,name='teacher_stats'),
     path('teacher_set_work',views.teacher_set_work,name='teacher_set_work'),
     path('teacher_print_flashcards',views.teacher_print_flashcards,name='teacher_print_flashcards'),
     path('teacher_download_pdf_from=<str:date_from>&to=<str:date_to>',views.teacher_download_pdf,name='teacher_download_pdf'),
-    path('play', views.play, name='play'),
-    path('play_all',views.play_all,name='play_all'),
+    
+
     path('create_attempt', views.create_attempt, name='create_attempt'),
     path('student',views.student,name='student'),
     path('student_play',views.student_play,name='student_play'),
@@ -21,24 +21,7 @@ urlpatterns = [
     path('admin_assign_students',views.admin_assign_students,name='admin_assign_students'),
     path('admin_remove_students',views.admin_remove_students,name='admin_remove_students'),
     path('admin_stats',views.admin_stats,name='admin_stats')
-
-
     ]
-"""
-    path('teach', views.teach, name='teach'),
-    
-    path('remove_students', views.remove_students, name='remove_students'),
-    path('stats', views.stats, name='stats'),
-    path('stats/<str:student>',views.student_stats,name='student_stats'),
-    path('stats/<str:student>/flash', views.flash, name='flash'),
-    path('stats_set/<str:student>',views.student_stats_set,name='student_stats_set'),
-    path('stats_set/<str:student>/flash',views.flash_set,name='flash_set'),
-    path('class_flash',views.class_flash,name='class_flash'),
-    path('class_stats',views.class_stats,name='class_stats'),
-    path('student_view_stats_all',views.student_view_stats_all,name='student_view_stats_all'),
-    path('student_view_stats_set',views.student_view_stats_set,name='student_view_stats_set'),
-    """
-
 
 
     
